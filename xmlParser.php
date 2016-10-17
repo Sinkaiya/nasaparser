07e1de31c6739c2c09fbd234a035337eaf0936a4
Sergey Novikov <sinkaiya@gmail.com>
2016-10-17 13:00:22 +0300

    Подправил название переменной.

--- xmlParser.php
+++ xmlParser.php
@@ -2,8 +2,8 @@
 error_reporting(E_ALL);
 ini_set('display_errors',1);
 
-$languages = simplexml_load_file("https://www.nasa.gov/rss/dyn/lg_image_of_the_day.rss");
+$rssData = simplexml_load_file("https://www.nasa.gov/rss/dyn/lg_image_of_the_day.rss");
 
-var_dump($languages);
+var_dump($rssData);
 
 ?>
